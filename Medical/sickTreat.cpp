db3b9d24a831420c8355cb1c7de1f5b3ad92ac5c
99002488 <71541657+99002488@users.noreply.github.com>
2020-10-17 14:14:43 +0530

    Adding comments as per #1

--- Medical/sickTreat.cpp
+++ Medical/sickTreat.cpp
@@ -5,34 +5,34 @@
 #include "sickTreat.h"
 
 
-SickTreat::SickTreat():Medical(),type(""),
+SickTreat::SickTreat():Medical(),type(""),                                //default constructor
     variants(""){}
 
 
 SickTreat::SickTreat(int ID, std::string name,int concentration,std::string section,std::string mfgDate,std::string expDate,double price,std::string type,std::string variants):
     Medical(ID,name,concentration,section,mfgDate,expDate,price),type(type),variants(variants){
 
-    }
+    }                                                                     //parameterized constructor
 
 
 
-    int SickTreat::getID(){
+    int SickTreat::getID(){                                               //function which returns ID 
 
     return ID;
 
     }
-    std::string SickTreat::getType(){
+    std::string SickTreat::getType(){                                     //function which returns type of the medicine
 
     return type;
 
     }
-    std::string SickTreat::getName(){
+    std::string SickTreat::getName(){                                     //function which returns name of the medicine
 
     return name;
 
     }
 
-    double SickTreat::getPrice(){
+    double SickTreat::getPrice(){                                         //function which returns price of the medicine
 
     return price;
 
