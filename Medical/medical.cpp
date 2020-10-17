5d6878cb8168b2c3377a5333de42b3feaa6c351d
99002488 <71541657+99002488@users.noreply.github.com>
2020-10-17 14:23:37 +0530

    Adding comments as per #1

--- Medical/medical.cpp
+++ Medical/medical.cpp
@@ -4,6 +4,6 @@
 #include <string>
 
 
-Medical::Medical():ID(0),name(""),concentration(0),section(""),mfgDate(""),expDate(""),price(0.0){}
+Medical::Medical():ID(0),name(""),concentration(0),section(""),mfgDate(""),expDate(""),price(0.0){}                                                                                                                                                                          //default constructor
 
-Medical::Medical(int ID, std::string name,int concentration,std::string section,std::string mfgDate,std::string expDate, double price) : ID(ID),name(name),concentration(concentration),section(section),mfgDate(mfgDate),expDate(expDate),price(price) {}
+Medical::Medical(int ID, std::string name,int concentration,std::string section,std::string mfgDate,std::string expDate, double price) : ID(ID),name(name),concentration(concentration),section(section),mfgDate(mfgDate),expDate(expDate),price(price) {}                   //parameterized constructor
